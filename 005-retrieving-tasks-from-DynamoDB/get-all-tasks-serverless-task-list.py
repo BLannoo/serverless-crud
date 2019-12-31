f4bf74a9358ab1b3ba09b92b15721ea68bc570f1
BLannoo <brunolannoo@gmail.com>
2019-12-31 14:16:01 +0100

    retrieve the data from DynamoDB

---
+++ 005-retrieving-tasks-from-DynamoDB/get-all-tasks-serverless-task-list.py
@@ -0,0 +1,22 @@
+import boto3
+import json
+
+dynamodb = boto3.client('dynamodb')
+
+
+def lambda_handler(event, context):
+    response = dynamodb.scan(TableName='task-list')
+    tasks = list(
+        {
+            'description': item['description']['S'],
+            'isCompleted': item['isCompleted']['BOOL'],
+        }
+        for item in response['Items']
+    )
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": json.dumps(tasks)
+    }
